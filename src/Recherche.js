5178ba30f4c22af7707a0c9f14e484131b6731e2
Gnagna Faly Signate <gnagnafalys@gmail.com>
2026-05-15 17:33:34 +0000

    Lab 4 : API Flask avec endpoint GET /lignes

---
+++ src/Recherche.js
@@ -0,0 +1,27 @@
+import './Recherche.css';
+
+function Recherche({ valeur, onChange }) {
+  return (
+    <div className="recherche">
+      <div className="recherche-conteneur">
+        <input
+          type="text"
+          className="recherche-input"
+          placeholder="Rechercher une ligne (depart, arrivee)..."
+          value={valeur}
+          onChange={e => onChange(e.target.value)}
+        />
+        {valeur && (
+          <button
+            className="recherche-effacer"
+            onClick={() => onChange("")}
+          >
+            ✕
+          </button>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Recherche;
